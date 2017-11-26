114cfb03777e81d1c836baa79573853ae37cf1df
lallepot <me@lallepot.com>
2017-11-26 02:07:47 +0100

    basic working version.

--- FindFile.py
+++ FindFile.py
@@ -1,9 +1,10 @@
 import hashlib
 import os
 import shutil
+import sys
 
-root_path = '/Volumes/Small/Pictures/jpg'
-
+#root_path = '/Volumes/Small/Audio/'
+root_path = '/Volumes/Small/Documents/'
 #root_path = '/Users/lallepot/Desktop/testing'
 
 dubdir = root_path+"/dub"
@@ -13,9 +14,19 @@ file = ""
 
 dubs = 0
 total = 0
+workload = 0
 
-if not os.path.isdir(dubdir):
-    os.mkdir(dubdir)
+print root_path
+
+for root, dirs, filenames in os.walk(root_path):
+    for filename in filenames:
+        if root == dubdir:
+            continue
+        if filename[:1] == '.':
+            continue
+        workload = workload + 1
+        print "\rCalculating Workload:", workload,
+print ""
 
 for root, dirs, filenames in os.walk(root_path):
     # folder level
@@ -27,16 +38,20 @@ for root, dirs, filenames in os.walk(root_path):
             continue
         file = os.path.join(root, filename)
         hash = hashlib.md5(open(file, 'rb').read()).hexdigest()
-        print file
+        #print "\r", file,
         for x in dict:
             if x == file:
                 break
             else:
                 if dict[x] == hash:
+                    if not os.path.isdir(dubdir):
+                        os.mkdir(dubdir)
                     shutil.move(file, dubdir+'/'+filename)
                     dubs = dubs + 1
                     break
         dict.update({file: hash})
         total = total + 1
+        print "\rProgress:", total , "/", workload, "(Duplicates:", str(dubs)+")",
+print ""
 
 print "Checked Files: ", total,"\n", "Number of Duplicates:", dubs
